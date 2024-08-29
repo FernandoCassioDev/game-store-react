1725d7ac8ad5d62cafa052a9fca9a996dbbb91d1
Fernando Cassio <fernandocassiodev@gmail.com>
2024-08-29 13:43:10 -0300

    feat: cria componente home

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { BrowserRouter, Routes } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/navbar/Navbar'
-import Footer from './components/footer/Footer'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Navbar />
-        <Routes></Routes>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+        </Routes>
         <Footer />
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
       <div className="flex justify-center bg-cyberpunk-bg text-cyberpunk-text">
         <div className="container flex flex-col items-center py-4">
           <p className="text-xl font-bold">Game Store | Copyright: {data}</p>
-          <p className="text-lg">Conheça mais em:</p>
+          <p className="font-bold">Conheça mais em:</p>
           <div className="flex gap-2">
             <a href="https://linkedin.com/in/fernando-cassio" target="_blank" className="hover:text-cyberpunk-accent2 transition-colors duration-300">
               <LinkedinLogo size={32} />
