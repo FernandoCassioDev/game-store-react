c0f160071744b7c79fe8b01221a11615d14e6a92
Fernando Cassio <fernandocassiodev@gmail.com>
2024-08-29 13:43:21 -0300

    style: adiciona logo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,10 +4,13 @@ function Navbar() {
   return (
     <>
       <div className="w-full flex justify-center py-4 bg-cyberpunk-bg text-cyberpunk-text">
-        <div className="container flex justify-between text-lg">
-          <Link to="/home" className="text-2xl font-bold hover:text-cyberpunk-accent2 transition-colors duration-300">
-            Game Store
-          </Link>
+        <div className="container flex justify-between text-lg items-center">
+        <div className="flex justify-center items-center">
+          <img src="src\assets\img\gaming.png" alt="controle de videogame" className="w-10 mr-3"/>
+            <Link to="/home" className="text-2xl font-bold hover:text-cyberpunk-accent2 transition-colors duration-300">
+              Game Store
+            </Link>
+        </div>
           <div className="flex">
             <div className="flex gap-4 hover:text-cyberpunk-accent2 transition-colors duration-300">
               Produtos
