12ea25c4b6529d28848d4b6abf0dbed7813987e0
Fernando Cassio <fernandocassiodev@gmail.com>
2024-08-29 12:33:56 -0300

    chore: adiciona variaveis da peleta de cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,35 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyberpunk: {
+          bg: 'var(--cyberpunk-bg)',
+          primary: 'var(--cyberpunk-primary)',
+          secondary: 'var(--cyberpunk-secondary)',
+          accent: 'var(--cyberpunk-accent)',
+          accent2: 'var(--cyberpunk-accent2)',
+          text: 'var(--cyberpunk-text)',
+        },
+        retrowave: {
+          bg: 'var(--retrowave-bg)',
+          primary: 'var(--retrowave-primary)',
+          secondary: 'var(--retrowave-secondary)',
+          accent: 'var(--retrowave-accent)',
+          accent2: 'var(--retrowave-accent2)',
+          text: 'var(--retrowave-text)',
+        },
+        modern: {
+          bg: 'var(--modern-bg)',
+          secondaryBg: 'var(--modern-secondary-bg)',
+          primary: 'var(--modern-primary)',
+          secondary: 'var(--modern-secondary)',
+          accent: 'var(--modern-accent)',
+          accent2: 'var(--modern-accent2)',
+          text: 'var(--modern-text)',
+        },
+      },
+    },
   },
   plugins: [],
 }
