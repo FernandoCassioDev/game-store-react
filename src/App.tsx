0081b63a453438e5487322714e062a3152a22bd1
Fernando Cassio <fernandocassiodev@gmail.com>
2024-08-30 22:52:52 -0300

    feat: adiciona rotas de componentes na App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
+import ListaCategorias from "./components/categorias/listacategorias/ListaCategorias";
+import FormCategoria from "./components/categorias/formcategoria/FormCategoria";
+import DeletarCategoria from "./components/categorias/deletarcategoria/DeletarCategoria";
 
 function App() {
   return (
@@ -10,7 +13,12 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/categorias" element={<ListaCategorias />} />
+          <Route path="/cadastrarcategoria" element={<FormCategoria />} />
+          <Route path="/editarcategoria/:id" element={<FormCategoria />} />
+          <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
         </Routes>
         <Footer />
       </BrowserRouter>
