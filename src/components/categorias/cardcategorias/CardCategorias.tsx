84e5afacd299f6f585d41aec80b355edcb5c294a
Fernando Cassio <fernandocassiodev@gmail.com>
2024-08-31 16:07:01 -0300

    style: estiliza componente CardTema

--- src/components/categorias/cardcategorias/CardCategorias.tsx
+++ src/components/categorias/cardcategorias/CardCategorias.tsx
@@ -9,21 +9,21 @@ function CardCategorias({ categoria }: CardCategoriasProps) {
 
   return (
   <div className="border flex flex-col rounded-xl overflow-hidden justify-between">
-    <header className="py-2 px-6 bg-indigo-800 text-white font-bold text-2xl">
+    <header className="py-2 px-6 bg-cyberpunk-bg text-cyberpunk-text font-bold text-2xl">
       Categoria
     </header>
-    <p className="p-8 text-3xl bg-slate-200 h-full">{categoria.tipo}</p>
+    <p className="p-8 text-3xl font-semibold bg-retrowave-bg text-white h-full">{categoria.tipo}</p>
     <div className="flex">
       <Link
         to={`/editarcategoria/${categoria.id}`}
-        className="w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800 flex items-center justify-center py-2"
+        className="w-full text-slate-100 bg-indigo-800 hover:bg-indigo-950 flex items-center justify-center py-2"
       >
         <button>Editar</button>
       </Link>
 
       <Link
         to={`/deletarcategoria/${categoria.id}`}
-        className="w-full text-slate-100 bg-red-400 hover:bg-red-700 flex items-center justify-center py-2"
+        className="w-full text-slate-100 bg-red-700 hover:bg-red-800 flex items-center justify-center py-2"
       >
         <button>Deletar</button>
       </Link>
